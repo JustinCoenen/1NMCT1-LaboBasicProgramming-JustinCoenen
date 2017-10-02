5e6ba4e46bc635f571129fc4448be90aa26dcd36
JustinCoenen <Justin.coenen@student.howest.be>
2017-10-02 11:10:39 +0200

    is het getal gelijk

--- labbo_02controlestructuur/oefeningen.py
+++ labbo_02controlestructuur/oefeningen.py
@@ -1,9 +1,12 @@
 #oefeningen - controle structuren
 
-getalA = int(input("geef een getal"))
-getalB = int(input("geef een tweede getal"))
+#Oefening 1
+getalA = int(input("geef een getal: "))
+getalB = int(input("geef een tweede getal: "))
 
 if (getalA==getalB):
     print("Het getal {0} en het getal {1} zijn gelijk".format(getalA, getalB))
 else:
     print("Het getal {0} en het getal {1} zijn verschillend".format(getalA, getalB))
+
+#Oefening 2
