6c466c74d00aa01c6e878b86cf04c877d1ecbee7
JustinCoenen <Justin.coenen@student.howest.be>
2017-10-23 12:21:48 +0200

    oefening 5 klaar

---
+++ Labo_05_Tuples_Dictionary_Sets/oef_05.py
@@ -0,0 +1,10 @@
+#oefening 5
+
+nmct = {"1NMCT": "101", "2NMCT":"88", "3NMCT": "77"}
+devine = {"1Devine": 123, "2Devine": 98, "3Devine": 55}
+
+howest = {}
+
+howest = nmct, devine
+
+print(howest)
